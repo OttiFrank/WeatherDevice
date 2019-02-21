0aa06c14389516aa1bcf878682d4501bba53c8da
OttiFrank <william.wirsen@live.se>
2019-02-21 11:23:29 +0100

    init wind speed branch

--- WeatherDevice/MainPage.xaml.cs
+++ WeatherDevice/MainPage.xaml.cs
@@ -6,7 +6,8 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.Devices.Enumeration;
 using Windows.Devices.Gpio;
-using Windows.Devices.I2c; 
+using Windows.Devices.I2c;
+using Windows.Devices.Spi;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -27,19 +28,22 @@ namespace WeatherDevice
     public sealed partial class MainPage : Page
     {
         I2cDevice si7021Sensor;
+        byte[] readBuffer = new byte[3];
+        byte[] writeBuffer = new byte[3] { 0x06, 0x00, 0x00 };
+        SpiDevice spi;
         DispatcherTimer timer;
 
         public MainPage()
         {
             this.InitializeComponent();
-
-            //InitGPIO();
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             StopScenario();
         }
+        
+
         async Task StartScenarioAsync()
         {
             string i2cDeviceSelector = I2cDevice.GetDeviceSelector();
@@ -90,10 +94,17 @@ namespace WeatherDevice
 
         void Timer_Tick(object sender, object e)
         {
+            //GetTempAndHumidity();
+            GetWindSpeed();
+        }
+
+        
+
+        private void GetTempAndHumidity()
+        {
             var command = new byte[1];
             var humidityData = new byte[2];
             var temperatureData = new byte[2];
-
             // Read humidity
             command[0] = 0xE5;
 
@@ -105,15 +116,50 @@ namespace WeatherDevice
 
             // Calculate and report the humidity
             var rawHumidityReading = humidityData[0] << 8 | humidityData[1];
-            var humidityRatio = rawHumidityReading / (float) 65536;
-            double humidity = -6 + (125 * humidityRatio);
+            double humidity = ((rawHumidityReading * 125) / (float)65536) - 6;
             CurrentHumidity.Text = humidity.ToString();
 
             // Calculate and report the temperature
             var rawTempReading = temperatureData[0] << 8 | temperatureData[1];
-            var tempRatio = rawTempReading / (float) 65536;
+            var tempRatio = rawTempReading / (float)65536;
             double temperature = (-46.85 + (175.72 * tempRatio));
             CurrentTemp.Text = temperature.ToString();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await StartSPI();
+            this.timer = new DispatcherTimer();
+            this.timer.Interval = TimeSpan.FromMilliseconds(500);
+            this.timer.Tick += Timer_Tick;
+            this.timer.Start();
+            base.OnNavigatedTo(e);
+        }
+
+        private async Task StartSPI()
+        {
+            try
+            {
+                var settings = new SpiConnectionSettings(0);
+                settings.ClockFrequency = 5000000;
+                settings.Mode = SpiMode.Mode0;
+
+                string spiAqs = SpiDevice.GetDeviceSelector("SPI0");
+                var deviceInfo = await DeviceInformation.FindAllAsync(spiAqs);
+                spi = await SpiDevice.FromIdAsync(deviceInfo[0].Id, settings);
+                System.Diagnostics.Debug.WriteLine("SPI: " + spi);
+            } catch (Exception ex)
+            {
+                throw new Exception("SPI Initialization Failed", ex); 
+            }
+        }
+        private void GetWindSpeed()
+        {
+            spi.TransferFullDuplex(writeBuffer, readBuffer);
+            var result = readBuffer[1] & 0x07;
+            result <<= 8;
+            result += readBuffer[2];
+            result >>= 1;
+            System.Diagnostics.Debug.WriteLine(result.ToString());
         }
     }
 }
