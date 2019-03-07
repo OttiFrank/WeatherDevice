d2e32d72af37e7b902f6365378416b4dc6330063
Martin Leijon <martinleeijon@gmail.com>
2019-03-07 11:16:05 +0100

    Init asp.NET MVC WebApp

---
+++ case_weatherapp/case_weatherapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace case_weatherapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
