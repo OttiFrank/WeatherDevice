5aa73ebdf4a876e0a75b6a78420f718dcd917792
William <william.wirsen@live.se>
2019-03-08 12:35:55 +0100

    Added wind and humidity chart to page

--- case_weatherapp/case_weatherapp/wwwroot/js/site.js
+++ case_weatherapp/case_weatherapp/wwwroot/js/site.js
@@ -2,125 +2,136 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var myChart;
+var tempChart, humidChart, windChart;
 var temperatures = [];
 var humidities = [];
+var winds = [];
 var timestampArray = [];
-var dateArray = [];
-var timeArray = [];
 loadWindData();
 loadTempHumid();
 
-function loadWindData()
-{
+function loadWindData() {
     $.ajax({
         url: '/Home/WindList',
         type: "GET",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
-        success: function(result){
+        success: function (result) {
             console.log(result);
+            setWindSpeed(result); 
         },
-        error: function(error) {
+        error: function (error) {
             console.log(JSON.stringify(error));
         }
     });
 }
-function loadTempHumid() 
-{
+function loadTempHumid() {
     $.ajax({
         url: '/Home/TempHumidList',
         type: "GET",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
-        success: function(result){
-            for(var i = 0; i < result.length; i++) {
-                
-                let timestamp = result[i].date; 
-                var date = timestamp.split("T"); 
-                dateArray.push(date[0]);
-                timeArray.push(date[1]);
-                timestampArray.push(date[0] + " " + date[1]); 
-
-                var temp = {
-                    temperature:result[i].temperature,
-                    date: timestampArray[i]
-                }
-                var humid = {
-                    humidity: result[i].humidity,
-                    date: result[i].date
-                }
-                temperatures.push(temp); 
-                humidities.push(humid);
-
-                AddData(myChart, temperatures[i].date, temperatures[i].temperature);
-            }
-            console.log(temperatures[2]);
-            console.log(dateArray);
-            console.log(timeArray);
-            console.log(timestampArray);
+        success: function (result) {
+            console.log(result); 
+            setTempHumid(result);   
         },
-        error: function(error) {
+        error: function (error) {
             console.log(JSON.stringify(error));
         }
     });
 }
+function setTempHumid(result) {
+    for (var i = 0; i < result.length; i++) {
+        let timestamp = result[i].date;
+        var date = timestamp.split("T");
+        timestampArray.push(date[0] + " " + date[1]);
 
-var ctx = document.getElementById("myChart").getContext('2d');
-myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
+        var temp = {
+            temperature: result[i].temperature,
+            date: timestampArray[i]
+        }
+        var humid = {
+            humidity: result[i].humidity,
+            date: timestampArray[i]
+        }
+        temperatures.push(temp);
+        humidities.push(humid);
+        AddData(tempChart, temperatures[i].date, temperatures[i].temperature);
+        AddData(humidChart, humidities[i].date, humidities[i].humidity);
+    }    
+}
+
+function setWindSpeed(result) {
+    let windTimeArray = [];
+    for (var i = 0; i < result.length; i++) {
+        let timestamp = result[i].date;
+        var date = timestamp.split("T");
+        windTimeArray.push(date[0] + " " + date[1]);
+
+        var wind = {
+            windspeed: result[i].windspeed,
+            date: windTimeArray[i]
+        }
+        winds.push(wind);
+        AddData(windChart, winds[i].date, winds[i].windspeed); 
+    }    
+}
+
+
+var ctx = document.getElementById("myTempChart").getContext('2d');
+tempChart = new Chart(ctx, {
+    type: 'line',
+    data: {
         labels: [],
         datasets: [{
-          label: 'Temperature in celsius',
-          data: [],
-          fill: true,
-          pointRadius: 0,
-          //borderColor: [
+            label: 'Temperature in celsius',
+            data: [],
+            fill: true,
+            pointRadius: 0,
+            //borderColor: [
             //'rgba(255,99,132,1)',
             //'rgba(54, 162, 235, 1)',
             //'rgba(255, 206, 86, 1)',
             //'rgba(75, 192, 192, 1)',
             //'rgba(153, 102, 255, 1)',
             //'rgba(255, 159, 64, 1)'
-          //],
-          borderWidth: 1
+            //],
+            borderWidth: 1
         }]
-      },
-        plugins: [{
-            beforeRender: function (x, options) {
-                var c = x.chart
-                var dataset = x.data.datasets[0];
-                var yScale = x.scales['y-axis-0'];
-                var yPos = yScale.getPixelForValue(0);
+    },
+    plugins: [{
+        beforeRender: function (x, options) {
+            var c = x.chart
+            var dataset = x.data.datasets[0];
+            var yScale = x.scales['y-axis-0'];
+            var yPos = yScale.getPixelForValue(0);
 
-                var gradientFill = c.ctx.createLinearGradient(0, 0, 0, c.height);
-                gradientFill.addColorStop(0, 'rgba(211, 33, 45, 1)');
-                gradientFill.addColorStop(yPos / c.height - 0.01, 'rgba(211, 33, 45, 0.2)');
-                gradientFill.addColorStop(yPos / c.height + 0.01, 'rgba(0, 70, 180, 0.2');
-                gradientFill.addColorStop(1, 'rgba(0, 70, 180, 1');
+            var gradientFill = c.ctx.createLinearGradient(0, 0, 0, c.height);
+            gradientFill.addColorStop(0, 'rgba(211, 33, 45, 1)');
+            gradientFill.addColorStop(yPos / c.height - 0.01, 'rgba(211, 33, 45, 0.2)');
+            gradientFill.addColorStop(yPos / c.height + 0.01, 'rgba(0, 70, 180, 0.2');
+            gradientFill.addColorStop(1, 'rgba(0, 70, 180, 1');
 
-                var model = x.data.datasets[0]._meta[Object.keys(dataset._meta)[0]].dataset._model;
-                model.backgroundColor = gradientFill;
-            },
-            beforeDraw: function (c) {
-                var data = c.data.datasets[0].data;
-                for (var i in data) {
-                    try {
-                        var bar = c.data.datasets[0]._meta[0].data[i]._model;
-                        if (data[i] > 0) {
-                            bar.borderColor = '#E82020';
-                        } else
-                            bar.borderColor = '#07C';
-                        }
-                        catch (ex) {
-                            console.log(ex.message);
-                        }
-                            console.log(data[i]);
-                        }
-                    }
-        }],
-      options: {
+            var model = x.data.datasets[0]._meta[Object.keys(dataset._meta)[0]].dataset._model;
+            model.backgroundColor = gradientFill;
+        },
+        beforeDraw: function (c) {
+            var data = c.data.datasets[0].data;
+            for (var i in data) {
+                try {
+                    var bar = c.data.datasets[0]._meta[0].data[i]._model;
+                    if (data[i] > 0) {
+                        bar.borderColor = '#E82020';
+                    } else
+                        bar.borderColor = '#07C';
+                }
+                catch (ex) {
+                    console.log(ex.message);
+                }
+            }
+        }
+    }],
+    options: {
         legend: {
             display: false
         },
@@ -134,24 +145,159 @@ myChart = new Chart(ctx, {
             intersect: false
         },
         scales: {
-          xAxes: [{
-            ticks: {
-                autoSkip: true,
-                maxTicksLimit: 20
-            },
-            distribution: 'auto'
-          }], 
-          yAxes: [{
-            ticks: {
-              beginAtZero:true
-            }
-          }]
+            xAxes: [{
+                ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 20
+                },
+                distribution: 'auto'
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
         }
-      }
-    });
-    
-function AddData(chart, label, data) 
-{
+    }
+});
+
+var ctx2 = document.getElementById("myHumidChart").getContext('2d');
+humidChart = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Humiditure in percentage',
+            data: [],
+            fill: true,
+            pointRadius: 0,
+            //borderColor: [
+            //'rgba(255,99,132,1)',
+            //'rgba(54, 162, 235, 1)',
+            //'rgba(255, 206, 86, 1)',
+            //'rgba(75, 192, 192, 1)',
+            //'rgba(153, 102, 255, 1)',
+            //'rgba(255, 159, 64, 1)'
+            //],
+            borderWidth: 1
+        }]
+    },
+    plugins: [{
+        beforeRender: function (x, options) {
+            var c = x.chart
+            var dataset = x.data.datasets[0];
+            var yScale = x.scales['y-axis-0'];
+            var yPos = yScale.getPixelForValue(0);
+
+            var gradientFill = c.ctx.createLinearGradient(0, 0, 0, c.height);
+            gradientFill.addColorStop(0, 'rgba(211, 33, 45, 1)');
+            gradientFill.addColorStop(yPos / c.height - 0.01, 'rgba(211, 33, 45, 0.2)');
+            gradientFill.addColorStop(yPos / c.height + 0.01, 'rgba(0, 70, 180, 0.2');
+            gradientFill.addColorStop(1, 'rgba(0, 70, 180, 1');
+
+            var model = x.data.datasets[0]._meta[Object.keys(dataset._meta)[0]].dataset._model;
+            model.backgroundColor = gradientFill;
+        },
+    }],
+    options: {
+        legend: {
+            display: false
+        },
+        responsive: true,
+        hover: {
+            mode: 'nearest',
+            intersect: false
+        },
+        tooltips: {
+            mode: 'nearest',
+            intersect: false
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 20
+                },
+                distribution: 'auto'
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx3 = document.getElementById("myWindSpeedChart").getContext('2d');
+windChart = new Chart(ctx3, {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Wind speed in m/s',
+            data: [],
+            fill: true,
+            pointRadius: 0,
+            //borderColor: [
+            //'rgba(255,99,132,1)',
+            //'rgba(54, 162, 235, 1)',
+            //'rgba(255, 206, 86, 1)',
+            //'rgba(75, 192, 192, 1)',
+            //'rgba(153, 102, 255, 1)',
+            //'rgba(255, 159, 64, 1)'
+            //],
+            borderWidth: 1
+        }]
+    },
+    plugins: [{
+        beforeRender: function (x, options) {
+            var c = x.chart
+            var dataset = x.data.datasets[0];
+            var yScale = x.scales['y-axis-0'];
+            var yPos = yScale.getPixelForValue(0);
+
+            var gradientFill = c.ctx.createLinearGradient(0, 0, 0, c.height);
+            gradientFill.addColorStop(0, 'rgba(211, 33, 45, 1)');
+            gradientFill.addColorStop(yPos / c.height - 0.01, 'rgba(211, 33, 45, 0.2)');
+            gradientFill.addColorStop(yPos / c.height + 0.01, 'rgba(0, 70, 180, 0.2');
+            gradientFill.addColorStop(1, 'rgba(0, 70, 180, 1');
+
+            var model = x.data.datasets[0]._meta[Object.keys(dataset._meta)[0]].dataset._model;
+            model.backgroundColor = gradientFill;
+        },
+    }],
+    options: {
+        legend: {
+            display: false
+        },
+        responsive: true,
+        hover: {
+            mode: 'nearest',
+            intersect: false
+        },
+        tooltips: {
+            mode: 'nearest',
+            intersect: false
+        },
+        scales: {
+            xAxes: [{
+                ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 20
+                },
+                distribution: 'auto'
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+function AddData(chart, label, data) {
     chart.data.labels.push(label);
     chart.data.datasets.forEach((dataset) => {
         dataset.data.push(data)
